e1de1acc1033e3bfe0e2431f57ce2e9dbdf9cd17
Khan kibria <khan.kibria@gmail.com>
2018-03-16 02:20:55 -0700

    clean up and some change in cli interface

--- ethdag.go
+++ ethdag.go
@@ -3,71 +3,93 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"os"
-	"strconv"
+	"path/filepath"
+	"unsafe"
 
-	"github.com/ethereum/go-ethereum/consensus/ethash"
+	eth "github.com/ethereum/go-ethereum/consensus/ethash"
 )
 
 const (
 	blocksPerEpoch = 30000
 )
 
+var (
+	endian     string
+	pStatsFlag *bool
+	pOutDir    *string
+	seedhash   []byte
+)
+
 func main() {
 	var err error
 
-	pDagCnt := flag.Uint64("r", 1, "Number of dags to generate. Maximum 16 dags.")
-	pBlockFlag := flag.Bool("b", false, "Set this flag if the number is a block number.")
-	pEpochFlag := flag.Bool("e", false, "Set this flag if the number is an epoch.")
-	pStatsFlag := flag.Bool("s", false, "Stats only, do not generate.")
-	pOutDir := flag.String("o", "ethdag", "Output directory prefix.")
+	pDatasetCnt := flag.Uint64("r", 1, "Number of datasets to generate. Maximum 16 dags.")
+	pBlock := flag.Uint64("b", math.MaxUint64, "Block number. Epoch number must not be specified.")
+	pEpoch := flag.Uint64("e", math.MaxUint64, "Epoch number. Block number must not be specified.")
+	pStatsFlag = flag.Bool("s", false, "Stats only, do not generate dataset.")
+	pOutDir = flag.String("o", "eth-dataset", "Output directory.")
 
 	flag.Usage = func() {
 		fmt.Printf("Usage of %s:\n", os.Args[0])
-		fmt.Printf("    -r -b|e -s -o outDir number\n")
+		fmt.Printf("    -r [-b block|-e epoch] -s -o outDir\n")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
 	flag.Parse()
 
-	if (flag.NArg() != 1) || (*pBlockFlag == *pEpochFlag) || (*pDagCnt < 1) || (*pDagCnt > 16) {
+	if (flag.NArg() != 0) || (*pBlock == *pEpoch) || (*pDatasetCnt < 1) || (*pDatasetCnt > 16) {
 		flag.Usage()
 	}
 
-	var num uint64
-
-	if num, err = strconv.ParseUint(flag.Arg(0), 10, 64); err != nil {
+	if (*pBlock == math.MaxUint64) && (*pEpoch == math.MaxUint64) {
 		flag.Usage()
 	}
 
-	var block uint64
-	var epoch uint64
-
-	if *pEpochFlag {
-		epoch = num
-		block = epoch * blocksPerEpoch
+	if (*pBlock != math.MaxUint64) && (*pEpoch != math.MaxUint64) {
+		flag.Usage()
 	}
 
-	if *pBlockFlag {
-		block = num
-		epoch = block / blocksPerEpoch
+	if *pEpoch != math.MaxUint64 {
+		*pBlock = *pEpoch * blocksPerEpoch
+	} else {
+		*pEpoch = *pBlock / blocksPerEpoch
 	}
 
-	fmt.Fprintf(os.Stdout, "Block = %v\n", block)
-	fmt.Fprintf(os.Stdout, "Epoch = %v\n", epoch)
+	fmt.Fprintf(os.Stdout, "Block = %v\n", *pBlock)
+	fmt.Fprintf(os.Stdout, "Epoch = %v\n", *pEpoch)
 
-	// sizes
-	fmt.Fprintf(os.Stdout, "Cache size = %v\n", ethash.CacheSize(block))
-	fmt.Fprintf(os.Stdout, "Dataset size = %v\n", ethash.DatasetSize(block))
+	if !isLittleEndian() {
+		endian = ".be"
+	}
 
-	seedhash := ethash.SeedHash(block)
-	fmt.Fprintf(os.Stdout, "seedhash = %v\n", seedhash)
-
-	if !*pStatsFlag {
-		for i := uint64(0); i < *pDagCnt; i++ {
-			e := epoch + i
-			ethash.MakeDataset((e)*blocksPerEpoch, fmt.Sprintf("%s-%v", *pOutDir, e))
+	for i := uint64(0); i < *pDatasetCnt; i++ {
+		e := *pEpoch + i
+		b := e * blocksPerEpoch
+		// sizes
+		seedhash = eth.SeedHash(b)
+		fmt.Fprintf(os.Stdout, "\nseedhash(%v) = %x\n", e, seedhash)
+		fmt.Fprintf(os.Stdout, "Cache size = %v\n", eth.CacheSize(b))
+		fmt.Fprintf(os.Stdout, "Dataset size = %v\n", eth.DatasetSize(b))
+		if !*pStatsFlag {
+			eth.MakeDatasetFinalize(b, *pOutDir)
+			if err = fixName(e); err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
+}
+
+func isLittleEndian() bool {
+	n := uint32(0x01020304)
+	return *(*byte)(unsafe.Pointer(&n)) == 0x04
+}
+
+func fixName(e uint64) error {
+	filename := fmt.Sprintf("full-R%d-%x%s", eth.GetRev(), seedhash[:8], endian)
+	oldPath := filepath.Join(*pOutDir, filename)
+	newPath := filepath.Join(*pOutDir, fmt.Sprintf("epoch-%v-full", e))
+	return os.Rename(oldPath, newPath)
 }
---
+++ ethhash/sizes.go
@@ -0,0 +1,29 @@
+// place this file in the ethash directory of the geth source
+// and rename to sizes.go to expose the following functions
+
+package ethash
+
+import "math"
+
+// CacheSize is exported version of cacheSize
+func CacheSize(block uint64) uint64 {
+	return cacheSize(block)
+}
+
+// DatasetSize is exported version of datasetSize
+func DatasetSize(block uint64) uint64 {
+	return datasetSize(block)
+}
+
+// GetRev returns algorith revision
+func GetRev() int {
+	return algorithmRevision
+}
+
+// MakeDatasetFinalize is same as MakeDataset(). It generates a new ethash dataset and
+// optionally stores it to disk. It additionally finalizes to close the file.
+func MakeDatasetFinalize(block uint64, dir string) {
+	d := dataset{epoch: block / epochLength}
+	d.generate(dir, math.MaxInt32, false)
+	d.finalizer()
+}
