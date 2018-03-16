877a0f9d91736b3e5ba1d96e34e486bcfef031c5
Khan kibria <khan.kibria@gmail.com>
2018-03-15 18:57:22 -0700

    added size printout.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ethdag
 Saves dag on disk, uses geth code.
+
+##Installing
+`go get github.com/kkibria/ethdag`
+
+will install the package and dependency geth. Copy the file `size.go.txt` in the ethash directory of the geth source tree and rename it to `sizes.go`. This will expose the cacheSize and datasetSize functions.
+
+##Compile
+Running `go install` will compile and install the executable in $GOPATH/bin dierctory. 
--- ethdag.go
+++ ethdag.go
@@ -14,25 +14,27 @@ const (
 )
 
 func main() {
-	repeatPtr := flag.Uint64("r", 1, "Number of dags to generate. Maximum 16 dags.")
-	blockFlagPtr := flag.Bool("b", false, "Set this flag if the number is a block number.")
-	epochFlagPtr := flag.Bool("e", false, "Set this flag if the number is an epoch.")
-	outDirPtr := flag.String("o", "ethdag", "Output directory prefix.")
+	var err error
+
+	pDagCnt := flag.Uint64("r", 1, "Number of dags to generate. Maximum 16 dags.")
+	pBlockFlag := flag.Bool("b", false, "Set this flag if the number is a block number.")
+	pEpochFlag := flag.Bool("e", false, "Set this flag if the number is an epoch.")
+	pStatsFlag := flag.Bool("s", false, "Stats only, do not generate.")
+	pOutDir := flag.String("o", "ethdag", "Output directory prefix.")
 
 	flag.Usage = func() {
 		fmt.Printf("Usage of %s:\n", os.Args[0])
-		fmt.Printf("    -r -b|e -o outDir number\n")
+		fmt.Printf("    -r -b|e -s -o outDir number\n")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
 	flag.Parse()
 
-	if (flag.NArg() != 1) || (*blockFlagPtr == *epochFlagPtr) || (*repeatPtr < 1) || (*repeatPtr > 16) {
+	if (flag.NArg() != 1) || (*pBlockFlag == *pEpochFlag) || (*pDagCnt < 1) || (*pDagCnt > 16) {
 		flag.Usage()
 	}
 
-	var err error
 	var num uint64
 
 	if num, err = strconv.ParseUint(flag.Arg(0), 10, 64); err != nil {
@@ -42,23 +44,30 @@ func main() {
 	var block uint64
 	var epoch uint64
 
-	if *epochFlagPtr {
+	if *pEpochFlag {
 		epoch = num
 		block = epoch * blocksPerEpoch
 	}
 
-	if *blockFlagPtr {
+	if *pBlockFlag {
 		block = num
 		epoch = block / blocksPerEpoch
 	}
 
 	fmt.Fprintf(os.Stdout, "Block = %v\n", block)
 	fmt.Fprintf(os.Stdout, "Epoch = %v\n", epoch)
+
+	// sizes
+	fmt.Fprintf(os.Stdout, "Cache size = %v\n", ethash.CacheSize(block))
+	fmt.Fprintf(os.Stdout, "Dataset size = %v\n", ethash.DatasetSize(block))
+
 	seedhash := ethash.SeedHash(block)
 	fmt.Fprintf(os.Stdout, "seedhash = %v\n", seedhash)
 
-	for i := uint64(0); i < *repeatPtr; i++ {
-		e := epoch + i
-		ethash.MakeDataset((e)*blocksPerEpoch, fmt.Sprintf("%s-%v", *outDirPtr, e))
+	if !*pStatsFlag {
+		for i := uint64(0); i < *pDagCnt; i++ {
+			e := epoch + i
+			ethash.MakeDataset((e)*blocksPerEpoch, fmt.Sprintf("%s-%v", *pOutDir, e))
+		}
 	}
 }
